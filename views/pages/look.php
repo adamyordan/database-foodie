cd7f625e674ef1b5d362ae5e954261ef9e975c02
Adam Jordan <adamyordan@gmail.com>
2016-05-13 23:21:16 +0700

    dynamic content in look.php according to role

--- views/pages/look.php
+++ views/pages/look.php
@@ -8,6 +8,7 @@
 
 			<div class="well">
 
+				<?php if ($data['user']->job == "Chef" || $data['user']->job == "Manager"): ?>
 
 				<table class="table table-mini">
 					<thead>
@@ -44,6 +45,74 @@
 						<?php endfor; ?>
 					</tbody> 
 				</table>
+
+			<?php elseif ($data['user']->job == "Kasir"): ?>
+
+				<table class="table table-mini">
+					<thead>
+						<tr>
+							<th>#</th>
+							<th>Nomor Nota</th>
+							<th>Waktu Bayar</th>
+							<th>Total</th>
+							<th>Kasir</th>
+							<th>Mode Bayar</th>
+							<th></th>
+						</tr>
+					</thead>
+					<tbody> 
+						<?php for($i = 1; $i <= 10; $i+=2): ?>
+						<tr> 
+							<td scope="row"><?php echo $i; ?></th> 
+							<td>ABC777</td> 
+							<td>13/05/2016 23:12:25</td> 
+							<td>13/05/2016 23:12:25</td> 
+							<td>60,000</td> 
+							<td>Tunai</td> 
+							<td><a href="#">Lihat</a></td>
+						</tr> 
+						<tr> 
+							<td scope="row"><?php echo $i+1; ?></th> 
+							<td>ABC779</td> 
+							<td>18/05/2016 23:12:25</td> 
+							<td>20/05/2016 23:12:25</td> 
+							<td>75,000</td> 
+							<td>Debit</td> 
+							<td><a href="#">Lihat</a></td>
+						</tr>
+						<?php endfor; ?>
+					</tbody> 
+				</table>
+
+			<?php elseif ($data['user']->job == "Staf"): ?>
+
+				<table class="table table-mini">
+					<thead>
+						<tr>
+							<th>#</th>
+							<th>Nomor</th>
+							<th>Waktu</th>
+							<th>Supplier</th>
+							<th>Staf</th>
+							<th></th>
+						</tr>
+					</thead>
+					<tbody> 
+						<?php for($i = 1; $i <= 10; $i++): ?>
+						<tr> 
+							<td scope="row"><?php echo $i; ?></th> 
+							<td>XYZ030</td> 
+							<td>05/05/2016 12:30</td> 
+							<td>Molly Special</td> 
+							<td>Olivia Domenica</td> 
+							<td><a href="#">Rincian</a></td>
+						</tr> 
+						<?php endfor; ?>
+					</tbody> 
+				</table>
+
+			<?php endif; ?>
+
 			</div>
 		</div>
 	</div>
