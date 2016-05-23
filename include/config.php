5beb5e445bc6add3a1cbdc891badb2d8952939f2
Adam Jordan <adamyordan@gmail.com>
2016-05-23 22:10:41 +0700

    added config.php

---
+++ include/config.php
@@ -0,0 +1,12 @@
+<?php
+
+// Config related to database connection
+define('DBNAME', "postgres");
+define('DBHOST', "localhost");
+define('DBPORT', "5432");
+define('DBUSER', "postgres");
+define('DBPASS', "postgres");
+
+define('SITENAME', "Foodie");
+
+?>
--- views/layout.php
+++ views/layout.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<title>Basis Data</title>
+	<title><?php echo SITENAME; ?></title>
 
 	<link rel="stylesheet" type="text/css" href="resources/css/vendor/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="resources/css/vendor/flat-ui.css">
